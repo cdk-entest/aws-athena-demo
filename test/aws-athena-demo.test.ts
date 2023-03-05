f261fdd44a43e3d7835e3312451e116c3ea850a3
entest <hai@entst.io>
2023-03-05 10:34:25 +0000

    Initial commit

---
+++ test/aws-athena-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsAthenaDemo from '../lib/aws-athena-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-athena-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsAthenaDemo.AwsAthenaDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
