c9604a6f8600f876e5dca888d31977359dc7d50b
entest <hai@entst.io>
2023-05-10 08:38:26 +0000

    update bin

--- bin/aws-athena-demo.ts
+++ bin/aws-athena-demo.ts
@@ -10,6 +10,8 @@ const app = new cdk.App();
 new AwsAthenaDemoStack(app, "AwsAthenaDemoStack", {
   destS3BucketName: config.destS3BucketName,
   sourceS3BucketName: config.sourceS3BucketName,
+  athenaWorkgroupName: "QueryWorkGroup",
+  sparkWorkgroupName: "SparkWorkGroup",
   env: {
     region: process.env.CDK_REGION_DEFAULT,
     account: process.env.CDK_ACCOUNT_DEFAULT,
@@ -20,6 +22,7 @@ new AwsAthenaDemoStack(app, "AwsAthenaDemoStack", {
 new DataScientistStack(app, "DataScientistStack", {
   userName: "data-scientist",
   athenaResultBucketArn: `arn:aws:s3:::${config.destS3BucketName}`,
+  athenaWorkgroupName: "QueryWorkGroup",
   sourceBucketArn: `arn:aws:s3:::${config.sourceS3BucketName}`,
   databaseName: "default",
   databasePermissions: [],
