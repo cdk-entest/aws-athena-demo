52b40cde590df1cb1b76be57afb00ddd0785469b
EC2 Default User <ec2-user@ip-10-0-1-110.ap-southeast-1.compute.internal>
2023-07-02 09:31:18 +0000

    clean

--- query/amazon_reviews_pds.sql
+++ query/amazon_reviews_pds.sql
@@ -15,8 +15,11 @@ create external table amazon_reviews_parquet_table (
 	review_body string,
 	review_date date,
 	`year` int
-) partitioned by (product_category string) row format serde 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe' stored as inputformat 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat' outputformat 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat' location "s3://amazon-reviews-pds/parquet/" tblproperties ("classification" = "parquet") -- msck repair table amazon_reviews_parquet
+) partitioned by (product_category string) row format serde 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe' stored as inputformat 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat' outputformat 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat' location "s3://amazon-reviews-pds/parquet/" tblproperties ("classification" = "parquet") 
+
+-- msck repair table amazon_reviews_parquet
 msck repair table amazon_reviews_parquet_table;
+
 -- create external table from tsv data  
 create external table amazon_reviews_tsv_table (
 	marketplace string,
@@ -37,7 +40,9 @@ create external table amazon_reviews_tsv_table (
 ) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' ESCAPED BY '\\' LINES TERMINATED BY '\n' stored as inputformat 'org.apache.hadoop.mapred.TextInputFormat' outputformat 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat' location "s3://amazon-reviews-pds/tsv/" tblproperties (
 	"classification" = "csv",
 	"skip.header.line.count" = "1"
-) -- create table and convert from csv to parquet 
+) 
+
+-- create table and convert from csv to parquet 
 create table if not exists ctas_table with (format = 'parquet') as
 select "marketplace",
 	"customer_id",
@@ -45,7 +50,9 @@ select "marketplace",
 	"product_id",
 	"product_title",
 	"star_rating"
-from "amazon_reviews_tsv_table" -- ctas create table in the athena result bucket with partitioned
+from "amazon_reviews_tsv_table" 
+
+-- ctas create table in the athena result bucket with partitioned
 	create table if not exists ctas_table_partitioned with (
 		format = 'parquet',
 		partitioned_by = array [ 'marketplace' ]
@@ -56,17 +63,25 @@ select "customer_id",
 	"product_title",
 	"star_rating",
 	"marketplace"
-from "amazon_reviews_tsv_table" -- show columsn and scheuma of a table 
+from "amazon_reviews_tsv_table" 
+
+-- show columsn and scheuma of a table 
 select *
 from information_schema.columns
-where table_name = 'amazon_reviews_parquet_table' -- count row 
+where table_name = 'amazon_reviews_parquet_table' 
+
+-- count row 
 select count(*) as num_row
-from "amazon_reviews_parquet_table" -- group by 
+from "amazon_reviews_parquet_table" 
+
+-- group by 
 select "customer_id",
 	sum("star_rating") as sum_rating
 from "amazon_reviews_parquet_table"
 group by "customer_id"
-order by sum_rating desc -- insert values 
+order by sum_rating desc 
+
+-- insert values 
 insert into "ctas_table"
 values(
 		'VN',
@@ -75,7 +90,9 @@ values(
 		'12345678',
 		'TCB',
 		5
-	) -- check output of insert 
+	) 
+
+-- check output of insert 
 select "marketplace",
 	"customer_id"
 from "ctas_table"
